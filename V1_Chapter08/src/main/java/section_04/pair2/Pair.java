0b5b33d34cbe2fa999183de2528c72d9d81f3f41
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2019-11-06 08:35:21 +0800

    ZhangGJ:Add V1_Chapter8:Generic Class,Method...

---
+++ V1_Chapter08/src/main/java/section_04/pair2/Pair.java
@@ -0,0 +1,23 @@
+package section_04.pair2;
+
+import lombok.Data;
+
+/**
+ * @author Cay Horstmann
+ * @version 1.00 2004-05-10
+ */
+@Data
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair() {
+        first = null;
+        second = null;
+    }
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+}
